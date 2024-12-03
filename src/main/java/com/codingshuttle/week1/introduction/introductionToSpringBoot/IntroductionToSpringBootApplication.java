2a25b045ed61d3192a43d83e7b1aecf4b040c2fa
naveenShekharGit <naveenshekharcp@gmail.com>
2024-12-03 11:47:50 +0530

    1.2 Setting up

---
+++ src/main/java/com/codingshuttle/week1/introduction/introductionToSpringBoot/IntroductionToSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.codingshuttle.week1.introduction.introductionToSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntroductionToSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntroductionToSpringBootApplication.class, args);
+		System.out.println("This is my first code of spring boot applications");
+	}
+
+}
